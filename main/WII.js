482ab3432cb4690130b70b50b779634779552d2f
trialrun2 <zanem@ksu.edu>
2022-10-02 17:01:57 -0500

    Login, html, not connecting to css for some reason

--- main/WII.js
+++ main/WII.js
@@ -33,7 +33,7 @@ var port = process.env.PORT || 1337;
 var fs = require('fs');
 
 http.createServer(function (req, res) {
-    fs.readFile('views/home.html', function (err, data) {
+    fs.readFile('views/Login.html', function (err, data) {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(data);
         return res.end();
