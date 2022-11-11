ca44699b14f57dbcc949abebbef5f61ee3a8ffaa
trialrun2 <zanem@ksu.edu>
2022-11-11 13:09:24 -0600

    login kind of

--- ExpressApp1/routes/index.js
+++ ExpressApp1/routes/index.js
@@ -1,48 +1,62 @@
 var express = require('express');
 const db = require('../database');
-const login = require('../public/javascripts/login');
 var router = express.Router();
 
 // Get index page
 router.get('/', function (req, res, next) {
     var users = db.prepare("SELECT * FROM users").all();
-    res.render('index', { title: 'Data goes here', users: JSON.stringify(users) });
+    res.render('index', { title: 'Wildcat Intramural Inc.', users: JSON.stringify(users) });
 });
 
 // Get teams page
 router.get('/teams', function (req, res, next) {
-    res.render('teams', { title: 'Data goes here' });
+    res.render('teams', { title: 'Teams' });
 });
 
 // Get leagues page
 router.get('/leagues', function (req, res, next) {
-    res.render('leagues', { title: 'Data goes here' });
+    res.render('leagues', { title: 'Leagues' });
 });
 
 // Get about page
 router.get('/about', function (req, res, next) {
-    res.render('about', { title: 'Data goes here' });
+    res.render('about', { title: 'About' });
 });
 
 // Get login page
 router.get('/login', function (req, res, next) {
     var users = db.prepare("SELECT * FROM users").all();
     console.log('Now on login page')
-    res.render('login', { title: 'Data goes here', users: (users) });
+    res.render('login', { title: 'Login', msg: '', users: (users) });
 });
 
 // Post from login page
 router.post('/login', function (req, res, next) {
+    var users = db.prepare("SELECT * FROM users").all();
     var email = req.body.email;
     var pass = req.body.password;
     console.log(email, pass);
-    res.redirect('./home')
+
+    var user = db.prepare("SELECT * FROM users WHERE email = ?").get(email);
+    console.log('user', user);
+
+    if (!user) {
+        res.render('login', { title: 'Login', msg: 'Incorrect Usename/Password. Please try again.', users: (users) });
+    }
+    if (pass === user.password_hash) {
+        res.redirect('./home');
+    }
+    else {
+        res.render('login', { title: 'Login', msg: 'Incorrect Username/Password. Please try again.', users: (users) });
+    }
+
+    
 });
 
 
 // Get sign-up page
 router.get('/signup', function (req, res, next) {
-    res.render('signup', { title: 'Data goes here' });
+    res.render('signup', { title: 'Sign Up' });
 });
 
 // Post from sign-up page
@@ -57,17 +71,17 @@ router.post('/signup', function (req, res, next) {
 
 // Get home page
 router.get('/home', function (req, res, next) {
-    res.render('home', { title: 'Data goes here' });
+    res.render('home', { title: 'Home' });
 });
 
 // Get stats page
 router.get('/stats', function (req, res, next) {
-    res.render('stats', { title: 'Data goes here' });
+    res.render('stats', { title: 'Stats' });
 });
 
 // Get rules page
 router.get('/rules', function (req, res, next) {
-    res.render('rules', { title: 'Data goes here' });
+    res.render('rules', { title: 'Rules' });
 });
 
 module.exports = router;
