7c6579fe574919eb6d82b688dea078a6835514cb
trialrun2 <zanem@ksu.edu>
2022-11-10 18:12:41 -0600

    finally figured out how to post to routes.js

--- ExpressApp1/public/javascripts/login.js
+++ ExpressApp1/public/javascripts/login.js
@@ -7,12 +7,37 @@ const views = require('./views');
  * @param {http.ServerResponse} res - the response object
  */
 
-function login(req, res) {
+/*function login(req, res) {
     var email = db.prepare("SELECT * FROM users ORDER BY email").all();
     var pug = views['login'](email);
     res.setHeader('Content-Type', "text/pug");
     res.setHeader('Content-Length', pug.length);
     res.end(pug);
+}*/
+
+//module.exports = login;
+
+/** @function createSession
+* A helper method invoked when session creation is
+* successful.  The request should have an object
+* as its body parameter with username and password set.
+* @param {http.IncomingMessage} req - the request object
+ * @param {http.ServerResponse} res - the response object
+*/
+
+function createSession(email, password) {
+    console.log(username, password);
+    var user = db.prepare("SELECT * FROM users WHERE email = ?").get(email);
+    console.log('user', user);
+    if (!user) return failure(req, res, "Usename/Password not found.  Please try again.");
+    bcrypt.compare(password, user.cryptedPassword, (err, result) => {
+        if (err) return serveError(req, res, 500, err);
+        console.log('result', result);
+        if (result) success(req, res, user);
+        else return failure(req, res, "Username/Password not found. Please try again.");
+
+    });
+
 }
 
-module.exports = login;
+module.exports = createSession;
--- ExpressApp1/routes/index.js
+++ ExpressApp1/routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 const db = require('../database');
+const login = require('../public/javascripts/login');
 var router = express.Router();
 
-// Get home page
+// Get index page
 router.get('/', function (req, res, next) {
     var users = db.prepare("SELECT * FROM users").all();
     res.render('index', { title: 'Data goes here', users: JSON.stringify(users) });
@@ -26,13 +27,33 @@ router.get('/about', function (req, res, next) {
 // Get login page
 router.get('/login', function (req, res, next) {
     var users = db.prepare("SELECT * FROM users").all();
-    res.render('login', { title: 'Data goes here' });
+    console.log('Now on login page')
+    res.render('login', { title: 'Data goes here', users: (users) });
 });
+
+// Post from login page
+router.post('/login', function (req, res, next) {
+    var email = req.body.email;
+    var pass = req.body.password;
+    console.log(email, pass);
+    res.redirect('./home')
+});
+
 
 // Get sign-up page
 router.get('/signup', function (req, res, next) {
     res.render('signup', { title: 'Data goes here' });
 });
+
+// Post from sign-up page
+router.post('/signup', function (req, res, next) {
+    var fname = req.body.fname;
+    var lname = req.body.lname;
+    var email = req.body.email;
+    var pass = req.body.password;
+    console.log(fname, lname, email, pass);
+    res.redirect('./home')
+})
 
 // Get home page
 router.get('/home', function (req, res, next) {
